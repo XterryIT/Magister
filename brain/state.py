b6cc0df2e6d50a8347711906f6f09c25287ea102
Ptashkohrai Andrii <vptashka2@gmail.com>
2026-07-09 11:42:46 +0200

    refactor: remove unused and deprecated helper functions from codebase

--- brain/state.py
+++
@@ -1,9 +0,0 @@
-from typing_extensions import TypedDict, Annotated
-from langgraph.graph.message import add_messages
-
-class IcedentAgentState(TypedDict):
-    incedent: list
-    messages: Annotated[list, add_messages]
-    report: str
-    escalate: bool # Флаг: если True, L1 молчит и передает дело на L2
-    target_ip: str # IP для которого сработал триггер
